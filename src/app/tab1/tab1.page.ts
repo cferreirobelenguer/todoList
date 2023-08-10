39c975202e67fa8b069b2ca0df77ccee4e25f422
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-08-10 20:31:41 +0200

    localStorage configuration

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {TodoItem} from './todoItem';
 
 @Component({
@@ -6,22 +6,40 @@ import {TodoItem} from './todoItem';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   public data:string = "";
   public todoData: TodoItem[] = [];
+  public localStorageData :TodoItem[] = [];
   public selectedIndex: number | null = null;
   constructor(
   ) {}
+
+  ngOnInit(): void {
+    const storedValue = localStorage.getItem('key');
+    //put data in the localStorage
+    if (storedValue !== null) {
+      this.localStorageData = JSON.parse(storedValue)
+    }
+    
+  }
   public handleChange = (event: any) => {
+    //input data
     this.data = event.target.value;
     const newItem: TodoItem = {
       isFinish: false,
       data: this.data
     };
-    this.todoData.push (newItem);
+    this.localStorageData.push (newItem);
+    //put the changes in the localStorage
+    localStorage.setItem("key", JSON.stringify(this.localStorageData));
+    const storedValue = localStorage.getItem('key');
+    if (storedValue !== null) {
+      this.localStorageData = JSON.parse(storedValue)
+    }
   }
 
   public labelClick = (index: number) => {
-    this.todoData[index].isFinish = true;
+    //when the user press the event, the task finished
+    this.localStorageData[index].isFinish = true;
   }
 }
